ed97e83fd297edf059f224d1923df452db67d211
Mohamed Salim <blackdjine@gmail.com>
2026-05-17 12:29:41 +0200

    Save current responsive product page progress

--- app/produit/page.tsx
+++ app/produit/page.tsx
@@ -136,7 +136,7 @@ export default function ProduitPage() {
 
     <div className="bg-[#f8f5ef] rounded-3xl p-4 shadow-lg">
 
-      <div className="space-y-4">
+      <div className="flex gap-4">
 
         <img
           src={activeImage}
@@ -247,7 +247,7 @@ export default function ProduitPage() {
           {selectedProduct.images &&
             selectedProduct.images.length > 0 && (
 
-              <div className="flex gap-3 flex-wrap">
+              <div className="flex flex-col gap-3">
 
                 {selectedProduct.images.map(
                   (

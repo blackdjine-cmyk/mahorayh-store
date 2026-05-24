b2a5bf47266e8bb76f36593d5ceeac6c158d8363
Mohamed Salim <blackdjine@gmail.com>
2026-05-24 13:59:28 +0200

    Add SEO technical setup with robots and sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://mahorayh-store.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://mahorayh-store.vercel.app/produit",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://mahorayh-store.vercel.app/panier",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://mahorayh-store.vercel.app/resultats",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://mahorayh-store.vercel.app/success",
+      lastModified: new Date(),
+    },
+  ];
+}

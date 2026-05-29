c7895e8ca27640c24ef7cb3ec04420ac4da487b9
Mohamed Salim <blackdjine@gmail.com>
2026-05-29 02:41:20 +0200

    Refine order details responsive layout and timeline

--- app/compte/commandes/[id]/page.tsx
+++ app/compte/commandes/[id]/page.tsx
@@ -55,7 +55,7 @@ export default function CommandeDetailPage() {
   console.log("STATUT =", commande.statut);
 
   return (
-  <div className="min-h-screen bg-gray-100 overflow-x-hidden">
+  <div className="min-h-screen bg-gray-100 px-3 py-6 overflow-x-hidden">
   <div className="max-w-4xl mx-auto bg-white rounded-3xl shadow-xl p-6 sm:p-8">
 
         <button
@@ -98,9 +98,8 @@ export default function CommandeDetailPage() {
 
     <div className="relative flex items-center justify-between w-full">
 
-       <div className="absolute left-6 right-6 top-5 h-1 bg-gray-300 rounded-full">
-        <div
-          className={`h-full bg-green-500 rounded-full transition-all duration-500
+       <div className="absolute left-[12%] right-[12%] top-5 h-1 bg-gray-300 rounded-full">
+        <div className={`h-full bg-green-500 rounded-full transition-all duration-500
             ${
               commande.statut === "en_attente"
                 ? "w-0"
